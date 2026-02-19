a57458eea58b9e76a2d9be08ff864f0b25bf9380
Huzaifa Zahid <huzaifazahid445@gmail.com>
2026-02-19 10:09:45 +0500

    Create user.rb
    added user model with name email and password validations
    
    password requires regex checks with One uppercase, One lowercase letter, a digit and a special character.

---
+++ app/models/user.rb
@@ -0,0 +1,33 @@
+class User < ApplicationRecord
+  has_secure_password
+
+  validates :username,
+            presence: true, uniqueness: true
+  validates :email, presence: true,
+            uniqueness: true
+
+  validates :password, presence: true,
+            length: { minimum: 8 },
+            if: :password_present_and_changed?
+  validate :password_requirements_are_met,
+           if: :password_present_and_changed?
+
+  private
+
+  def password_present_and_changed?
+    password.present? && (new_record? || will_save_change_to_password_digest?)
+  end
+
+  def password_requirements_are_met
+    rules = {
+      "must contain at least one lowercase letter" => /[a-z]+/,
+      "must contain at least one uppercase letter" => /[A-Z]+/,
+      "must contain at least one digit" => /\d+/,
+      "must contain at least one special character" => /[^A-Za-z0-9]+/
+    }
+
+    rules.each do |message, regex|
+      errors.add(:password, message) unless password.match(regex)
+    end
+  end
+end
