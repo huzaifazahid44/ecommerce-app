a7ff2ca6c98315dd882826b7ce99d50d3fd55de5
Huzaifa Zahid <huzaifazahid445@gmail.com>
2026-02-20 11:24:46 +0500

    Update application.js
    removed tailwindplus-elements that break turbo working functionality

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,6 +1,5 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import * as Turbo from "@hotwired/turbo-rails"
-window.Turbo = Turbo  // expose globally so browser console can inspect it
+window.Turbo = Turbo
 
 import "controllers"
-import "@tailwindplus/elements"
